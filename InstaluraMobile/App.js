2e7e8f48d1352fe4b7abb51f197ccd901262c1eb
Diogo Carvalho <diogofelipec@gmail.com>
2019-03-14 00:17:39 -0300

    adding photo

--- InstaluraMobile/App.js
+++ InstaluraMobile/App.js
@@ -7,32 +7,29 @@
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
 
-import React, {Component} from "react";
-import {
-  FlatList
-} from "react-native";
+import React, { Component } from "react";
+import { FlatList } from "react-native";
 import Post from "./components/Post";
 
 export default class App extends Component {
+  constructor() {
+    super();
+    this.state = { posts: [] };
+  }
+  componentDidMount() {
+    fetch("http://10.0.2.2:8080/api/public/fotos/rafael")
+      .then(resp => resp.json())
+      .then(json => this.setState({ posts: json }));
+  }
+
   render() {
-
-    const posts = [
-      {id: 1, name: "Diogo"},
-      {id: 2, name: "Tainá"},
-      {id: 3, name: "Marcela"},
-      {id: 4, name: "Thiago"}
-    ];
-
+    console.log(this.state.posts)
     return (
-
-        <FlatList
-            renderItem={({item}) => (
-                <Post post={item}/>
-            )}
-            data={posts}
-            keyExtractor={(item, index) => 'list-item' + item.id + index}
-        />
-
+      <FlatList
+        renderItem={({ item }) => <Post post={item} />}
+        data={this.state.posts}
+        keyExtractor={(item, index) => "list-item" + item.id + index}
+      />
     );
   }
 }
--- InstaluraMobile/components/Post.js
+++ InstaluraMobile/components/Post.js
@@ -1,17 +1,12 @@
-import React, {Component} from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  Dimensions,
-  Image
-} from "react-native";
+import React, { Component } from "react";
+import { StyleSheet, Text, View, Dimensions, Image, TouchableOpacity } from "react-native";
 
 const width = Dimensions.get("screen").width;
 
 const styles = StyleSheet.create({
   photo: {
-    width: width
+    width: width,
+    height: width
   },
   container: {
     flexDirection: "row",
@@ -24,28 +19,43 @@ const styles = StyleSheet.create({
     height: 40,
     borderRadius: 20,
     marginStart: 5,
-    marginEnd: 5,
-
+    marginEnd: 5
+  },
+  likeButton: {
+    width: 40,
+    height: 40
+  },
+  footer: {
+    margin: 10
   }
 });
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { post: this.props.post };
+  }
+
+  likeButtonPressed(){
+    console.warn("IIIIIIIIIIIIIIU")
+  }
+
   render() {
+    const { post } = this.state;
+
     return (
-        <View>
-          <View style={styles.container}>
-            <Image
-                source={require("../assets/images/placeholder.png")}
-                style={styles.userPicture}
-            />
-            <Text>{this.props.post.name}</Text>
-          </View>
-          <Image
-              source={require("../assets/images/placeholder.png")}
-              style={styles.photo}
-          />
+      <View>
+        <View style={styles.container}>
+          <Image source={{ uri: post.urlPerfil }} style={styles.userPicture} />
+          <Text>{post.loginUsuario}</Text>
         </View>
-    )
+        <View>
+          <Image source={{ uri: post.urlFoto }} style={styles.photo} />
+        </View>
+        <TouchableOpacity style={styles.footer} onPress={this.likeButtonPressed.bind(this)}>
+          <Image source={{ uri: post.urlFoto }} style={styles.likeButton} />
+        </TouchableOpacity>
+      </View>
+    );
   }
 }
-
